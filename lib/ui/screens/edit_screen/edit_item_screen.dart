830c777f8477f205aae46dd9b28e766d003c462e
Sh4dow-502 <101212418+Sh4dow-502@users.noreply.github.com>
2024-11-28 18:05:25 -0600

    UI usando datos reales

--- lib/ui/screens/edit_screen/edit_item_screen.dart
+++ lib/ui/screens/edit_screen/edit_item_screen.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/domain/models/task_model.dart';
 import 'package:task_app/ui/screens/components/components.dart';
 
-class EditItemScreen extends StatelessWidget {
-  final String item;
+class EditItemScreen extends StatefulWidget {
+  final Object item;
   const EditItemScreen({super.key, required this.item});
+
+  @override
+  State<EditItemScreen> createState() => _EditItemScreenState();
+}
+
+class _EditItemScreenState extends State<EditItemScreen> {
+  String _itemType = "";
+  @override
+  void initState() {
+    if (widget.item is TaskModel) {
+      _itemType = "task";
+    } else {
+      _itemType = "category";
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,16 +36,16 @@ class EditItemScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              item == "task" ? "Edit Task" : "Edit Category",
+              _itemType == "task" ? "Edit Task" : "Edit Category",
               style: const TextStyle(
                 fontSize: 33,
                 fontWeight: FontWeight.w500,
               ),
             ),
             const SizedBox(height: 30),
-            InputItem(itemType: item),
+            InputItem(itemType: _itemType),
             const SizedBox(height: 15),
-            item == "task" ? const SelectDateTime() : const SizedBox(),
+            _itemType == "task" ? const SelectDateTime() : const SizedBox(),
           ],
         ),
       ),
